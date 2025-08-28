ade6e6c752dcf398a7d6423a753eda58b4b7af5c
toojessy <jnkamienski@my.waketech.edu>
2025-08-28 10:04:50 -0400

    Create Temperature Converter.java

---
+++ Temperature Converter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Temp {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String getInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+    public static void main(String[] args) {
+
+        String input = getInput("Enter temperature in Fahrenheit: ");
+        try {
+            double fahrenheit = Double.parseDouble(input);
+            double celsius = (fahrenheit - 32) * 5 /9;
+            System.out.println(fahrenheit + "°F is " + celsius + "°C");
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid numeric temperature.");
+        }
+        scanner.close();
+
+    }
+
+}
